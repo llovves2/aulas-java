febaa5d5528c1b7b47132d5ae51e7e6f4da661e8
llovves2 <concadivino@gmail.com>
2026-01-20 18:43:17 -0300

    add arquivos

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// TESTE JS, OPERAÇOES MATEMATICAS
+
+// ATVD1
+// let n1 = 60;
+// let n2 = 20; 
+
+// console.log("Soma: " + (n1+n2));
+// console.log("Adição: " + (n1-n2));
+// console.log("Multiplicação: " + (n1*n2));
+// console.log("Divisão: " + (n1/n2));
+// console.log("Resto: " + (n1%n2));
+
+// ATVD2
+// let base = 10;
+// let altura = 50;
+
+// console.log("Retangulo: " + (base*altura));
+
+// ATVD3
+// let vreal = 1000;
+// let txcambio = 5.0;
+
+// console.log("Conversão: " + (vreal/txcambio));
+
+// ATVD4
+// let n1 = 2;
+// let n2 = 3;
+
+// console.log("Elevação :" + (n1**n2));
+
+// ATVD5
+// let n1 = 7;
+// let n2 = 8;
+// let n3 = 9;
+// let n4 = 10;
+// let qntdn = 4;
+
+// console.log("A Medeia é: " + ((n1+n2+n3+n4)/qntdn))
+
+// //ATVD6
+// let c = 11;
+
+// console.log("Fahrenheit: " + ((c* 9/5)+ 32));
+// //ATVD7
+// let n1 = 15 ;
+// let n2 =  4;
+
+// console.log("Resto é: " + (n1%n2));
