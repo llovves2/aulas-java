8aea0b919b759f47039ddf87f6046ecd9b2a49aa
llovves2 <concadivino@gmail.com>
2026-01-20 20:57:38 -0300

    add arquivos

--- src/index.js
+++ src/index.js
@@ -16,17 +16,19 @@
 
 // console.log("Retangulo: " + (base*altura));
 
-// ATVD3
+//ATVD3
 // let vreal = 1000;
 // let txcambio = 5.0;
+// let Conversão = vreal/txcambio;
 
-// console.log("Conversão: " + (vreal/txcambio));
+// console.log(Conversão);
 
 // ATVD4
 // let n1 = 2;
 // let n2 = 3;
+// let elevação = n1**n2;
 
-// console.log("Elevação :" + (n1**n2));
+// console.log (elevação);
 
 // ATVD5
 // let n1 = 7;
@@ -34,15 +36,75 @@
 // let n3 = 9;
 // let n4 = 10;
 // let qntdn = 4;
+// let media = (n1+n2+n3+n4)/qntdn;
 
-// console.log("A Medeia é: " + ((n1+n2+n3+n4)/qntdn))
+// console.log (`A media das notas é: ${media}`);
 
 // //ATVD6
 // let c = 11;
-
-// console.log("Fahrenheit: " + ((c* 9/5)+ 32));
+// let f = ((c* 9/5)+32);
+// console.log (f);
 // //ATVD7
 // let n1 = 15 ;
 // let n2 =  4;
 
 // console.log("Resto é: " + (n1%n2));
+
+
+// INTERPOLAÇAO NO CODIGO.
+// `${Variavel}`
+//EX: console.log (`A media das notas é: ${media}`);
+                                                                                                                            
+//ATVD 8
+
+// let vpd = 1200
+// let porc = 0.15
+// let desconto = vpd*porc;
+// let valordc = vpd-desconto;
+
+// console.log(`O desconto ficou no valor de: R$${desconto}, eo Valor final de: R$${valordc}.`);
+
+// //ATVD9
+// let n1 = 40;
+// let suc = n1 +1;
+// let antc = n1 -1;
+
+// console.log(`O sucessor de ${n1} é : ${suc}, e o Antecessor é : ${antc}.`)
+
+//ATVD10
+
+// let peso = 100.00;
+// let altura = 1.76;
+// let imc = peso/altura**2;
+
+// console.log(imc);
+
+//ATVD11
+
+// let num =10;
+// console.log(num===10);
+
+//ATVD12
+
+// let idade = 10;
+// if (idade >= 18){
+//     console.log("Vc está na maior idade.")
+// }else{
+//     console.log("Vc é de menor.")
+// }
+
+//ATVD13
+
+// let a = "Javascript";
+// let b = "javascript";
+
+// console.log(a==b);
+
+//ATVD14
+
+// let valor = 2;
+// if (valor === 0){
+//     console.log("esse numero é zero")
+// }else{
+//    console.log("esse numero nao é zero")
+// }
