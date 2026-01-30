0b3a1525f5de1db83e0dfbd34bb7262ae7a259c8
llovves2 <concadivino@gmail.com>
2026-01-29 21:00:51 -0300

    add arquivos

--- src/switch.js
+++ src/switch.js
@@ -67,56 +67,189 @@
 // }
 
 //ATVD02
-let meses = "Dezembro";
-switch (meses){
-    case "janeiro":{
-        console.log("A estação refente a janeiro é o Verão");
-        break;
-    }
-    case "Fevereiro":{
-        console.log("A estação refente a Fevereiro é o Verão");
-        break;
-    }
-    case "Março":{
-        console.log("A estação refente a Março é o Verão");
-        break;
-    }
-    case "Abril":{
-        console.log("A estação refente a Abril é o Outono");
-        break;
-    }
-    case "Maio":{
-        console.log("A estação refente a Maio é o Outono");
-        break;
-    }
-    case "Junho":{
-        console.log("A estação refente a Junho é o Outono");
-        break;
-    }
-    case "Julho":{
-        console.log("A estação refente a Julho é o Inverno");
-        break;
-    }
-    case "Agosto":{
-        console.log("A estação refente a Agosto é o Inveno");
-        break;
-    }
-    case "Setembro":{
-        console.log("A estação refente a Setembro é o Inverno");
-        break;
-    }
-    case "Outubro":{
-        console.log("A estação refente a Outubro é o Primavera");
-        break;
-    }
-    case "Novembro":{
-        console.log("A estação refente a Novembro é o Primavera");
-        break;
-    }
-    case "Dezembro":{
-        console.log("A estação refente a Dezembro é o Primavera");
-        break;
-    }default:{
-        console.log("Nao identificado.");
-    }
-}
+// let meses = "janeiro";
+// switch (meses){
+//     case "janeiro":{
+//         console.log("A estação refente a janeiro é o Verão");
+//         break;
+//     }
+//     case "Fevereiro":{
+//         console.log("A estação refente a Fevereiro é o Verão");
+//         break;
+//     }
+//     case "Março":{
+//         console.log("A estação refente a Março é o Verão");
+//         break;
+//     }
+//     case "Abril":{
+//         console.log("A estação refente a Abril é o Outono");
+//         break;
+//     }
+//     case "Maio":{
+//         console.log("A estação refente a Maio é o Outono");
+//         break;
+//     }
+//     case "Junho":{
+//         console.log("A estação refente a Junho é o Outono");
+//         break;
+//     }
+//     case "Julho":{
+//         console.log("A estação refente a Julho é o Inverno");
+//         break;
+//     }
+//     case "Agosto":{
+//         console.log("A estação refente a Agosto é o Inveno");
+//         break;
+//     }
+//     case "Setembro":{
+//         console.log("A estação refente a Setembro é o Inverno");
+//         break;
+//     }
+//     case "Outubro":{
+//         console.log("A estação refente a Outubro é o Primavera");
+//         break;
+//     }
+//     case "Novembro":{
+//         console.log("A estação refente a Novembro é o Primavera");
+//         break;
+//     }
+//     case "Dezembro":{
+//         console.log("A estação refente a Dezembro é o Primavera");
+//         break;
+//     }default:{
+//         console.log("Nao identificado.");
+//     }
+// }
+
+//ATVD03
+// let num1 = 10;
+// let num2 = 5;
+// let operaçao = "*" ;
+// switch(operaçao){
+//     case "+":{
+//         console.log(num1+num2);
+//         break;
+//     }
+//     case "-":{
+//         console.log(num1-num2);
+//         break;
+//     }
+//     case "*":{
+//         console.log(num1*num2);
+//         break;
+//     }
+//     case "/":{
+//         console.log(num1/num2);
+//         break;
+//     }default:{
+//         console.log("operação n identificada. ");
+//     }
+// }
+//ATVD04
+// let nota = "F";
+// switch (nota){
+//     case "A":{
+//         console.log("Excelente.");
+//         break;
+//     }
+//     case "B":{
+//         console.log("Bom.");
+//         break;
+//     }
+//     case "C":{
+//         console.log("Regular.");
+//         break;
+//     }
+//     case "D":{
+//         console.log("Ruim.");
+//         break;
+//     }
+//     case "F":{
+//         console.log("Reprovado.");
+//         break;
+//     }default:{
+//         console.log("Nota não identificada.")
+//         break;
+//     }
+// }
+//ATVD05
+// let codigo = 5;
+// switch(codigo){
+//     case 1:{
+//         console.log("X-burguer R$ 7,00");
+//         break;
+//     }
+//     case 2:{
+//         console.log("X-Salada R$ 8,00");
+//         break;
+//     }
+//     case 3:{
+//         console.log("Batata Frita R$ 10,00");
+//         break;
+//     }
+//     case 4:{
+//         console.log("Refigerante R$ 7,00");
+//         break;
+//     }default:{
+//         console.log("Codigo não identifica.");
+//         break;
+//     }
+// }
+//ATVD06
+
+// let letra = "B";
+// switch(letra){
+//     case "A":
+//     case "E":
+//     case "I":
+//     case "O":
+//     case "U":{
+//         console.log("Essa letra é uma vogal.");
+//         break;
+//     }default:{
+//         console.log("Essa letra é uma consoante ou outro caractere. ");
+//         break;
+//     }
+// }
+
+//ATVD7
+
+// let plano = "diamante";
+// switch(plano){
+//     case "Bronze":{
+//         console.log("Custo beneficio, Disponibilidade de 1 tela, Resolução HD");
+//         break;
+//     }
+//     case "Prata":{
+//         console.log("Disponibilidade de 2 a 3 telas, sem anuncios, Resolução full HD");
+//         break;
+//     }
+//     case "Ouro":{
+//         console.log("Disponibilidade até 5 tela, sem anuncios, Resolução 4k, Baixar offline.");
+//         break;
+//     }default:{
+//         console.log("Plano não identificado.");
+//     }
+// }
+
+//ATVD08
+// let valorR = 1000;
+// let moeda = "GBP";
+// switch(moeda){
+//     case "USD":{
+//         console.log(valorR*0.19);
+//         break;
+
+
+
+//     }
+//     case "EUR":{
+//         console.log(valorR*0.16);
+//         break;
+//     }
+//     case "GBP":{
+//         console.log(valorR*7.17);
+//         break;
+//     }
+    
+//     }
