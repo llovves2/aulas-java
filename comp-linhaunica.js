6b3cfd553363edfccf668b3d24075585b49cdcbe
llovves2 <concadivino@gmail.com>
2026-02-09 21:04:52 -0300

    add arquivos

---
+++ comp-linhaunica.js
@@ -0,0 +1,6 @@
+// ? = então
+//: = else/ se n
+//Ex:
+// let num1 = -1;
+// num1 = num1 <0 ? 10 :num1;         
+// console.log(num1);     
---
+++ estretura-repetiçao.js
@@ -0,0 +1,28 @@
+// principais estruturas de rep
+// for = para(para saber quantas vzs o bloco deve ser executado)
+//while = if
+//do...while = faça enquanto (ser executado pelo menos uma vez independente da condiçao)
+// break = parar o codigo.
+//continue = escolher entre um indice e outro./ignorar
+//foreach = é usado para percorrer arrays.
+//EX:
+// for ( let i = -10 ; i < 5; i++){
+//     if (i==3 || i ==4){
+//         continue;
+//     }
+//     if (i == 2){
+//         break;
+//     }
+//     console.log(`Valor de i: ${i}`);
+// }
+
+// let op = 0;
+// while (op == 0){
+//     console.log("baguio doido");
+//     op = 2;
+// }    
+// // 
+// do{
+//     console.log("baguio doido2")
+// } while (op==0);
+
