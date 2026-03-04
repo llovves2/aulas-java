0369cc0a24820d8259a55bdd68abe6926fd8ca79
llovves2 <concadivino@gmail.com>
2026-03-04 20:37:56 -0300

    add arquivos

--- estretura-repetiçao.js
+++ estretura-repetiçao.js
@@ -192,20 +192,21 @@
 
     
 
-//ATVD04
 
-// let contador = 0;
-// let num = 0;
-// let soma = 0;
-// let media = 5;
-// while(contador < 6){
-//     // console.log(contador);
-//   soma = soma + num;
-//     contador++;
-//   console.log(soma);
+//ATVD04
+// let media = 0;
+// while (true){
+// let num1 = 10;
+// let num2 = 14;
+// let num3 = 12;
+// let num4 = 100;
+// let num5 = 45;
+
+
+// media = (num1 + num2 +num3 +num4+ num5)/2;
+// break;
 
 // }
-// media /= soma;
 // console.log(media);
 
 //ATVD 05
@@ -216,6 +217,13 @@
 //     num++; 
 // }
 // console.log("primeiro numero multiplo de 7 maior que 50 é: " + num);
+
+// let cont = 0;
+// while((cont *7)<50){
+//     ++cont;
+// }
+// console.log(cont);
+
 //ATVD 06
 
 // let i = 0;
@@ -223,14 +231,19 @@
 //    i = Math.floor(Math.random()*10)+1;
 //    console.log(i);
 // }
+
+
+
 //ATVD07
 // let cont = 1;
 // let soma = 0;
 // while(soma <=50){
 //     soma+=cont;
-//     console.log(`Adicionando ${cont}, soma atual: ${soma}`);
+//     ++cont
+//     co-nsole.log(`Adicionando ${cont}, soma atual: ${soma}`);
 // }
 // console.log("Valor total: " + soma);
+
 
 //ATVD08
 // let i = 1;
@@ -248,8 +261,314 @@
 // }
 
 //ATVD10
-let cond = false;
-while(cond){
-    console.log("Ola mundo. ");
-    cond = true;
-}
+// let cond = false;
+// while(cond){
+//     console.log("Ola mundo. ");
+//     cond = true;
+// }
+
+//ATVD01 dowhile
+
+// let senha = parseInt(prompt ("Di"));
+// do{
+//     if(senha !== "123"){
+//         console.log("digite a senha mais uma vez.");
+        
+//     }
+
+// }while (senha !== "123"){
+//     console.log("senha correta ");
+    
+
+// }
+
+//interaçao com usuario 
+
+//prompt =caixa de dialgo para usuario insira um valor, retornando com o valor que usuario colocou como uma(string)
+//  ou null (nulo/vazio) caso a interaçao seja cancelada.
+
+// alert = caixa de dialogo de informativo não retorna nada. apenas informativo para usuario.
+
+//confirm exibe pergunta com dois botoes ok para confirmar e cancelar para encerrar.
+
+// defer = conectar com html sem dar godé.
+
+// CASTING(conversão)
+
+// o casting é o processo de transformar um valor de um tipo de dado para outro. String 10 no number 10.
+//Number(): Metodo mais direto. converte o valor mantendo decimais se n conseguir converter retorna NAN.
+//Parselnt(): converte para um inteiro (descarta decimais). Ultil para converter  "10px" em 10.
+//parseFloat(): converte para numero com ponto flutuante(decimais).
+//String(): converte para texto
+//.to string() um metodo que pode ser chamado diretamente na variavel.
+
+
+//EX:
+
+// let num = parseInt(prompt("Digite um numero."));
+// while(num <100 || isNaN(num)){
+//     alert("Numero invalido");
+//     num = parseInt(prompt("Digite um numero."));
+// }
+// alert("ok gostoso.");
+
+// let time = 10;
+// let start = confirm("Deseja iniciar o conometro?");
+// while(start && time > 0){
+//     alert(`tempo : ${time}`);
+//     --time;
+//     if (time == 0 ){
+//         alert("lançar. ");
+//     }
+// }
+
+
+
+// let continuar = true
+// while (continuar) {
+//     let c = parseFloat(prompt("Digite a temperatura em °C: "));    
+//     let f = (c*9) /5 +32;
+//     alert("A temperatura em Faherenheit é : " + f);
+//     continuar = confirm("Deseja inserir uma nova temp? ");
+// }
+
+// let st = prompt("Digite um texto: ");
+//     for (let i = 0; i <st.length; i++){
+//         // console.log(st[i]);
+//         document.writeln(st[i] + "<br>");
+//     }
+
+//ATVD01 LOGICA COM INTERAÇAO DE USUARIO
+
+// let nota1 = parseFloat(prompt("Digite a nota 1"));
+// let nota2 = parseFloat(prompt("Digite a nota 2"));
+// let nota3 = parseFloat(prompt("Digite a nota 3"));
+// let nota4 = parseFloat(prompt("Digite a nota 4"));
+// let mediaAri = (nota1 + nota2 + nota3 + nota4)/4 ;
+
+// alert("sua media é igual: " + mediaAri);
+
+
+//ATVD03
+
+// let altura = parseFloat(prompt("Digite sua Altura"));
+// let peso = parseFloat(prompt("Digite sua Peso"));
+// let imc = peso/altura**2;
+// alert(`Seu Imc é : ${imc.toFixed(2)}`);
+
+//ATVD04
+
+// let num = parseInt(prompt("Digite um numero: "));
+// if (num <= 0){
+//     alert(`O numero ${num} é menor ou igual a 0 `);
+// }else{
+//     alert(`O numero ${num} é maior que 0 `);
+// }
+
+
+// //ATVD05
+//  let saldo = 10000; //parseFloat(prompt("Digite o valor do saldo: "));
+// let saque = parseInt(prompt("Digite o valor do saque: "));
+// if (saldo < saque){
+//     alert("Vc não tem saldo suficiente para o saque.");
+
+// }else{
+//     alert("Vc tem saldo suficiente para o saque.");
+// }
+
+//ATVD06
+//  let usuario = prompt("Digite o usuario: ");
+//  let senha = prompt("Digite a senha: ");
+// if(usuario === "Admin" && senha === "1234"){
+//     alert("login Aceito");
+// }else{
+//     alert("Login Recusado");
+// }
+ 
+//ATVD07
+// let diasemana = prompt("Digite o dia da semana");
+// if(diasemana === "sabado" || diasemana === "domingo"){
+//     alert(`${diasemana} é fim de semana`);
+// }else{
+//     alert(`${diasemana} Não é final de semana`);
+// }
+
+//ATVD08
+
+// let idadedog = parseInt(prompt("Digite a idade do dog: "));
+// let conversao = idadedog*7;
+// alert
+
+// (`Um cão com ${idadedog} anos de idade é equivalente a ${conversao} anos de idade humana. `);
+
+//ATVD09
+
+// let dia = parseInt(prompt("Digite um numero correspondente a o dia da semana: "));
+// switch (dia){
+//     case 1:{
+//         alert("Domingo");
+//         break;
+//     }
+//     case 2:{
+//         alert("Segunda");
+
+//         break;
+//     }
+//     case 3:{
+//         alert("Terça");
+//         break;
+//     }
+//     case 4:{
+//         alert("Quarta");
+//         break;
+//     }
+//     case 5:{
+//         alert("Quinta");
+//         break;
+//     }
+//     case 6:{
+//         alert("Sexta");
+//         break;
+//     }
+//     case 7:{
+//         alert("Sabado");
+//         break;
+//     }default:{
+//         alert("Não é um dia da semana. ");
+
+//     }
+// }
+
+//ATVD10
+// let meses = prompt("Digite um mes do ano");
+// switch (meses){
+//     case "janeiro":
+//     case "Fevereiro":
+//     case "Março":{
+//         alert("A estação refente a esse mes é o Verão");
+//         break;
+//     }
+    
+//     case "Abril":
+//     case "Maio":
+//     case "Junho":{
+//         alert("A estação refente a esse mes é o Outono");
+//         break;
+//     }
+//     case "Julho":
+//     case "Agosto":
+//     case "Setembto":{
+//         alert("A estação refente a esse mes é o Inverno");
+//         break;
+//     }
+    
+//     case "Outubro":
+//     case "Novembro":
+//     case "Dezembro":{
+//         alert("A estação refente a esse mes é o Primavera");
+//         break;
+    
+//     }default:{
+//         alert("Nao identificado.");
+//     }
+// }
+
+//ATVD11
+// let num1 = parseFloat(prompt("Digite um numero: "));
+// let num2 = parseFloat(prompt("Digite um numero: "));
+// let operaçao = "/" ;
+//             switch(operaçao){
+//     case "+":{
+//         alert(num1+num2);
+//         break;
+//     }
+//     case "-":{
+//         alert(num1-num2);
+//         break;
+//     }
+//     case "*":{
+//         alert(num1*num2);
+//         break;
+//     }
+//     case "/":{
+//         alert(num1/num2);
+//         break;
+//     }default:{
+//         alert("operação n identificada. ");
+//     }
+// }
+
+
+//ATVD12
+// let nota = prompt("Digite sua nota. ");
+// switch (nota){
+//     case "A":{
+//         alert("Excelente.");
+//         break;
+//     }
+//     case "B":{
+//         alert("Bom.");
+//         break;
+//     }
+//     case "C":{
+//         alert("Regular.");
+//         break;
+//     }
+//     case "D":{
+//         alert("Ruim.");
+//         break;
+//     }
+//     case "F":{
+//         alert("Reprovado.");
+//         break;
+//     }default:{
+//         alert("Nota não identificada.")
+//         break;
+//     }
+// }
+
+
+//ATVD13
+// let plano = prompt("Digite qual plano vc deseja. ");
+// switch(plano){
+//     case "Bronze"&&"bronze":{
+//         alert("Os beneficio do plano Bronze são: Custo beneficio, Disponibilidade de 1 tela, Resolução HD");
+//         break;
+//     }
+//     case "Prata"&&"prata":{
+//         alert("Os beneficio do plano Prata são: Disponibilidade de 2 a 3 telas, sem anuncios, Resolução full HD");
+//         break;
+//     }
+//     case "Ouro"&&"ouro":{
+//         alert("Os beneficio do plano Ouro são: Disponibilidade até 5 tela, sem anuncios, Resolução 4k, Baixar offline.");
+//         break;
+//     }default:{
+//         alert("Plano não identificado.");
+//     }
+// }
+
+//ATVD14
+// let cargo = prompt("Digite seu cargo. ");
+// switch (cargo){
+//     case "Admin" &&"admin":{
+//         alert("Olá Admin, Vc tem Ascesso total ao sistema. ");
+//         break;
+//     }
+//     case "Editor" &&"editor":{
+//         alert("Olá Editor, Vc tem ascesso a Edição de pastas no sistema. ");
+//         break;
+//     }
+//     case "Visitante" && "visitante  ":{
+//         alert("Olá Visitante, Vc pode Vizualizar superficialmente ao nosso sistema . ");
+//         break;
+//     }
+//     default:{
+//         alert("Cargo n identificado");
+//         break;
+//     }
+// }
+
+//ATVD 15
+// let i ;
+// for( i = 1;)
+
