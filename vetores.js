27cd516f8582a6b93dd82a226b0202891c9fd7b6
llovves2 <concadivino@gmail.com>
2026-03-16 20:52:36 -0300

    add arquivos

---
+++ vetores.js
@@ -0,0 +1,28 @@
+// indice do vertor inicia sempre em 0
+// identificador de vetor = []
+// tipagem heterogenia = [numero, String, true/false];
+// EX :
+// let vet = [1,2,3];
+// let vet2 = ["Ariel", "aiai"];
+// console.log(vet[2])
+
+//vet.push colocara um indice em ultimo no lugar do vetor
+//vet.pop metodo void do vetor, excluirar ultimo indice
+//vet unshift colocara um indice no inicio do vetor. 
+//vet.shift irar manter o indice.
+//vet.length ira mostrar qiantos indeces tem no vetor
+//vet.indexoff mostrará a posciçao do vetor
+
+//ATVD01
+// let vet = [1,2,3,4,5,6];
+// console.log(vet);
+
+//ATVD02
+// let vet = [1,2,3,4,5,6];
+// vet.pop();
+// console.log(vet);
+
+//ATVD03
+// let vet = [1, 2, 3, 4, 5, 6];
+// vet.unshift(10);
+// console.log(vet);
