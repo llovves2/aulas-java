14913ea11f2a9f1ad75d33c727a111131c61f7c2
llovves2 <concadivino@gmail.com>
2026-03-18 20:34:37 -0300

    add arquivos

--- vetores.js
+++ vetores.js
@@ -77,3 +77,63 @@
 
 // let frutas = ["maçã", "coco","morango", "banana"];
 // console.log(frutas.indexOf("banana"));
+
+
+//ATVD10
+
+// let animais = ["urso", "vaca", "mocó", "jegue"];
+// animais.reverse();
+// alert(animais);
+
+//ATVD11
+// let idades = [10, 11, 15, 16, 17];
+// for (i = 0; i < idades.length; i++){
+//     console.log(idades[i]);
+// }
+
+
+//ATVD12
+
+// let preços = [15, 18, 19 ,20];
+// let soma = 0;
+// for(i = 0; i < preços.length; i++){
+//     soma+=preços[i];
+    
+// }
+// console.log(soma);
+
+//ATVD13
+// let nomes = ["jagunços", "ohmygood", "ariel", "caixanildo"];
+// let contador = 0;
+// for (let i = 0; i < nomes.length; i++){
+//     if (nomes[i].length == 5){
+//         console.log(nomes[i]);
+//         contador++;
+//     }
+// }
+
+//ATVD14
+
+// let numeros = [100, 15, 17, 55, 14,13];
+// let limite = 50;
+// for(let i = 0; i < numeros.length; i++){
+//     if (numeros[i] > limite ){
+//         console.log(numeros[i]);
+       
+        
+//     }
+    
+// }
+
+//ATVD15
+// let numeros = [100, 15, 17, 55, 14,13];
+// let maior = Math.max(...numeros);
+// console.log(maior);
+
+
+//ATVD16
+
+// let vetO = [100, 15, 17, 55, 14,13];
+// let dobro = vetO.map(numero => numero * 2);
+// console.log(dobro);
+// console.log(vetO);
